7b20ac843cc52b18f34cced301d62de36da609e3
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-09-01 23:39:31 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,6 +2,13 @@ import socket  # noqa: F401
 from socket import create_server, socket as Socket
 
 
+class HttpResponseStatusCodes:
+    SUCCESSFUL = "HTTP/1.1 200 OK"
+
+
+END_RESPONSE = "\r\n\r\n"
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -16,7 +23,10 @@ def main():
         reuse_port=True,
     )
 
-    server_socket.accept()      # wait for client
+    client_socket, client_address = server_socket.accept()      # wait for client
+    client_socket.sendall(
+        __data=f"{HttpResponseStatusCodes.SUCCESSFUL}{END_RESPONSE}".encode()
+    )
 
 
 if __name__ == "__main__":

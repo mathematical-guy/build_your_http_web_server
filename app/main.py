ed8a967812e2e7ede6612c461a602e4f4217a0e6
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-09-01 23:28:24 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 import socket  # noqa: F401
+from socket import create_server, socket as Socket
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    _HOST = "localhost"
+    _PORT = 4221
+
     # Uncomment this to pass the first stage
     #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket: Socket = create_server(
+        address=(_HOST, _PORT),
+        reuse_port=True,
+    )
+
+    server_socket.accept()      # wait for client
 
 
 if __name__ == "__main__":

b3e71ace744fb36a6302e1b1ba68808860a0d6a7
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-09-02 23:40:20 +0530

    Added Response Constructor

--- app/main.py
+++ app/main.py
@@ -8,11 +8,49 @@ class HttpResponseStatusCodes:
     NOT_FOUND = "HTTP/1.1 404 Not Found"
 
 
+class ResponseConstructor:
+    def __init__(self, headers=None, body=None):
+        if headers is None:
+            self.headers = {"Content-Type": "text/plain"}
+        else:
+            self.headers = headers
+
+        self.body = body
+        self.response = None
+        self.status_code = HttpResponseStatusCodes.SUCCESSFUL
+
+    def __append_headers(self):
+        headers = ""
+        for key, value in self.headers.items():
+            headers += f"{key}: {value}\r\n"
+
+        self.response = self.response + "\r\n" + headers
+
+    def __append_body(self):
+        self.response = self.response + "\r\n" + self.body
+
+    def __append_status_code(self):
+        self.response = self.status_code
+
+    def construct(self) -> str:
+        """
+        constructs response adding status code, headers and body (if needed)
+        :return: str
+        """
+
+        self.__append_status_code()
+        self.__append_headers()
+        if self.body:
+            self.__append_body()
+
+        return self.response
+
+
 class RequestAnalyzer:
     def __init__(self, request: str | bytes):
         self.headers = None
         self.method = None
-        self.url_path = None
+        self.url_path: str | None = None
         self.response = HttpResponseStatusCodes.SUCCESSFUL
 
         if isinstance(request, bytes):
@@ -28,8 +66,17 @@ class RequestAnalyzer:
         self.method, self.url_path, http_version = request_line.split(' ')
 
     def parse_request(self):
-        if self.url_path not in ["/", "index.html"]:
-            self.response = HttpResponseStatusCodes.NOT_FOUND
+        if self.url_path in ["/", "index.html"]:
+            self.response = HttpResponseStatusCodes.SUCCESSFUL
+
+        elif 'echo' in self.url_path:
+            # HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
+            echo_string: str = self.url_path.split('/')[-1]
+            response_constructor = ResponseConstructor(body=echo_string)
+            self.response = response_constructor.construct()
+
+        if END_RESPONSE not in self.response:
+            self.response += END_RESPONSE
 
         return self.response
 
@@ -62,7 +109,7 @@ def main():
     response = request_analyzer.parse_request()
 
     # HTTP version MESSAGE HEADING STATUS CODE  \r\n\r\n
-    client_socket.sendall(f"{response}{END_RESPONSE}".encode())
+    client_socket.sendall(f"{response}".encode())
 
 
 if __name__ == "__main__":
